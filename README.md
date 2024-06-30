d2f8b3679dd4673598cec6bd41dd76031c077a16
Gabriel Tinsley <gabrieltinsley@u.boisestate.edu>
2024-06-30 16:02:25 -0700

    Server is created to see website

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # redesigned-sniffle
 This repository is for creating a game
+
+### Getting Started
+This project can be started by installing the node modules.
+
+Type `npm install` to get the node modules into the command line.
+
+Type `npm start` to get onto the server to start editing data.
+
+As of right now the only area where you can edit what is on the server in in views/index.pug. To convert html to pug you can use https://html-to-pug.com/
+
+Once you have finished doing `npm start` click on this link to see the website http://localhost:3000/
+
---
+++ src/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import sql from 'sqlite3'
+
+const sqlite3 = sql.verbose()
+
+const db = new sqlite3.Database(':memory:')
+
+const app = express()
+app.use(express.static('public'))
+app.set('views', 'views')
+app.set('view engine', 'pug')
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', function (req, res) {
+    try {
+        console.log('GET called')
+        res.render('index')
+    } catch (err) {
+        res.status(404).send('Page not found')
+    }
+})
+
+/* Start the web server */
+app.listen(3000, function () {
+    console.log('Listening on port 3000...')
+})
